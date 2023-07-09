f143b888536e352b6efa89b048c764040a3d5ed6
dmytro.ivanov <dmytro.ivanov@form.com>
2023-07-09 19:25:00 +0300

    test

--- src/test/java/IssuesTabTest.java
+++ src/test/java/IssuesTabTest.java
@@ -7,13 +7,6 @@ public class IssuesTabTest extends BaseTest {
     @Test
     public void createNewIssueCheck() {
 
-        BasicSteps.clickOnRepoFromTheList();
-        SelectedRepositoryPage selectedRepositoryPage = new SelectedRepositoryPage(driver);
-        selectedRepositoryPage.goToIssueTab();
-        IssuesTab issuesTab = new IssuesTab(driver);
-        issuesTab.clickNewIssueButton();
-        IssueCreationTab issueCreationTab = new IssueCreationTab(driver);
-        assertTrue(issueCreationTab.findTitleInput().isDisplayed());
     }
 
 }
