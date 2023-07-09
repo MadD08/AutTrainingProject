31f27de8fd3c3d68fe51bda8c2887c35bb754a3a
dmytro.ivanov <dmytro.ivanov@form.com>
2023-07-09 18:45:38 +0300

    test

--- src/main/java/SelectedRepositoryPage.java
+++ src/main/java/SelectedRepositoryPage.java
@@ -14,6 +14,7 @@ public class SelectedRepositoryPage extends BasePage{
     }
 
     public WebElement findIssues() {
+        webDriverWait.until(elementToBeClickable(driver.findElement(issuesTabLocator)));
         return driver.findElement(issuesTabLocator);
     }
 
